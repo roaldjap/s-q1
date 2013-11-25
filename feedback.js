720f37ebc45111e5b73b3b98f69b2a90f6f0fd38
Roald Ignacio <me@apple.mac>
2013-11-26 01:46:51 +0800

    Confirmation if this is fine.

--- feedback.js
+++ feedback.js
@@ -1,17 +1,19 @@
 /**
  * Handles submitting the feedback form
  */
-(function (window, $) {
+ $(document).ready(function() 
+ {
 
-   function validateEmail(email) {
-        var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        return re.test(email);
-   }
+  function validateEmail(email) {
+        var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
+        return pattern.test(email);
+  }
+
 
   function initialiseForm() {
     var $form = $("form[name='feedback-form']");
 
-    var $submitButton = $("input[type='submit']");
+    var $submitButton = $(".btn-skiddoo");
 
     var $inputs = $form.find('input, textarea');
     var $errorField = $form.find('.error-field');
@@ -19,10 +21,10 @@
 
     var checked = $contactMe.prop('checked');
 
-    var  $firstname = $("input[name='firstname']").val();
-    var  $lastname = $("input[name='lastname']").val();
-    var  $email = $("input[name='email']").val();
-    var  $feedback = $("textarea[name='feedback']").val();
+    var firstname = $("input[name='firstname']").val();
+    var lastname = $("input[name='lastname']").val();
+    var email = $("input[name='email']").val();
+    var feedback = $("textarea[name='feedback']").val();
 
 // toggling active
     $inputs.focus(function() {
@@ -37,44 +39,42 @@
 
     //submit
     $submitButton.click(function() {
+
       $errorField.empty();
-      //$('#global-message').empty();
 
       //validate each field is filled.
       $inputs.each(function() {  
-        if ($(this).val() == "" || $(this).val() == null ){
+        if ($(this).val() === "" || $(this).val() === null ){
           $errorField.append('<li>'+ $(this).prop('placeholder') +' is required</li>');
         }
       });
         
-      //invalid input email address?     
-      if(!validateEmail($email)){
-        $errorField.append('<li>Email Address is invalid</li>');
+      //invalid input email address    
+      if(validateEmail(email) === false){
+        //$errorField.append('<li>Email Address is invalid</li>');
       }
       
       //if no errors  
-      if($errorField.children().length == 0){
+      if($errorField.children().length === 0){
         fields = {};
 
-        fields.firstname = $firstname;
-        fields.lastname = $lastname;
-        fields.email = $email;
-        fields.feedback = $feedback;
+        fields.firstname = firstname;
+        fields.lastname = lastname;
+        fields.email = email;
+        fields.feedback = feedback;
 
         //Subscribe must say thank you
         if ($('input[name="subscribe"]').is(':checked')) {
-          $.ajax({url:'provide-feedback', type:'PUT', data: {fields: JSON.stringify(fields)},
-              success:function (data) {
-                $('#global-message').html('Thank you ' + $firstname + ' for submitting your feedback.').css({"padding": "10px"});   
-                $('#global-message').html('Thank you for subscribing to our mailing list').css({"padding": "10px"});
-              }
-          });
+          $.ajax('/provide-feedback', {data: {fields: JSON.stringify(fields)}});
+          $('#global-message').empty();
+          $('#global-message').append('<p>Thank you ' + firstname + ' for submitting your feedback.</p>').css({"padding": "10px"});   
+          $('#global-message').append('<p>Thank you for subscribing to our mailing list</p>').css({"padding": "10px"});
           
-        }
-        else{
-          //TODO: do not understand here.
-          //$.ajax('/provide-feedback', {data: {fields: JSON.stringify(fields)}});
-          $('#global-message').html('Thank you ' + $firstname + ' for submitting your feedback.').css({"padding": "10px"});     
+        } 
+
+        else {
+          $.ajax('/provide-feedback', {data: {fields: JSON.stringify(fields)}});
+          $('#global-message').html('Thank you ' + firstname + ' for submitting your feedback.').css({"padding": "10px"});     
         }
          
     
@@ -86,5 +86,7 @@
 
   window.onload = function() {
     initialiseForm();
-  }
-})(window, jQuery);
+  };
+ });
+
+
